d5a436787554229e29a62b00918f921568f102b0
Benjamin ROUVILLE <brouville@webexpr.fr>
2022-10-13 17:22:43 +0200

    Init game

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState<any>();
+  const [scorePlayer, setScorePlayer] = useState<number>(0);
+  const [scoreBot, setScoreBot] = useState<number>(0);
+
+  useEffect(() => {
+    if(selectedItem){
+      const botValue = array[Math.floor(Math.random() * array.length)];
+      if(selectedItem.id == 'ciseaux' && botValue.id == 'feuille'){
+        setScorePlayer(scorePlayer + 1);
+      } else if(selectedItem.id == 'ciseaux' && botValue.id == 'pierre'){
+        setScoreBot(scoreBot + 1);
+      }
+
+    }
+  }, [selectedItem])
+
   const array = [
-    {src:"/img/ciseaux.png",alt:"ciseaux"},
-    {src:"/img/feuille.png", alt:"feuille" },
-    {src:"/img/pierre.png",alt:"pierre" }
+    {id:"ciseaux", src:"/img/ciseaux.png",alt:"ciseaux"},
+    {id: "feuille", src:"/img/feuille.png", alt:"feuille" },
+    {id: "pierre", src:"/img/pierre.png",alt:"pierre" }
   ];
   
   return (
-   
-
     <div className="App">
       <h1>Jeux du pierre - feuille - ciseau</h1>
+      <h2>Score player : {scorePlayer}</h2>
+      <h2>Score bot : {scoreBot}</h2>
       
-
-     <div className='images'>
-      {array.map((item)=>
-           <img key={item.src} src={item.src} alt={item.alt}/>
-          )};
-     {/*<img onClick src={"} alt=""/> 
-      <img onClick="" src={"/img/feuille.png" }  alt="feuille"/>
-      <img onClick="" src={"/img/pierre.png" } alt="pierre"/>*/}
-      
+      <div className='images'>
+        {array.map((item)=>
+          <img key={item.src} src={item.src} alt={item.alt} onClick={() =>setSelectedItem(item)}/>
+        )};
       </div>
     </div>
   );
