7f0eeac503c65815cdf58699d6f5f40d6ba8f527
HP <wiameabdouss06@gmail.com>
2022-10-13 17:02:56 +0200

    game

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import React, { useEffect } from 'react';
 import './App.scss';
 
 function App() {
-
+  const array = [
+    {src:"/img/ciseaux.png",alt:"ciseaux"},
+    {src:"/img/feuille.png", alt:"feuille" },
+    {src:"/img/pierre.png",alt:"pierre" }
+  ];
+  
   return (
+   
+
     <div className="App">
       <h1>Jeux du pierre - feuille - ciseau</h1>
       
+
+     <div className='images'>
+      {array.map((item)=>
+           <img key={item.src} src={item.src} alt={item.alt}/>
+          )};
+     {/*<img onClick src={"} alt=""/> 
+      <img onClick="" src={"/img/feuille.png" }  alt="feuille"/>
+      <img onClick="" src={"/img/pierre.png" } alt="pierre"/>*/}
+      
+      </div>
     </div>
   );
 }
