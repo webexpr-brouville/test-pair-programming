a521191535a0806cf17f225f29795ab45b86cb6c
Benjamin ROUVILLE <brouville@webexpr.fr>
2022-10-13 16:10:09 +0200

    Initialisation du projet

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import './App.scss';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Jeux du pierre - feuille - ciseau</h1>
+      
+    </div>
+  );
+}
+
+export default App;
